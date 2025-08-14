11c7707ab793c1046eaf77f3e38bcd57a284b99b
Christophe Tremblay <tremblay717@gmail.com>
2025-08-14 09:23:47 -0400

    first commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Netdata Linux Installation
+curl https://get.netdata.cloud/kickstart.sh > /tmp/netdata-kickstart.sh && sh /tmp/netdata-kickstart.sh
+
+
+curl https://get.netdata.cloud/kickstart.sh > /tmp/netdata-kickstart.sh && sh /tmp/netdata-kickstart.sh --stable-channel --claim-token NJ40WkbN7pT3Y6R_P039Jh38L1gnkqEfPVZugeKYIQH7gbLbLJevEonJe7Ao5M0PCwqwOX-toDgTknVMKTSdi5skhcBqg1DNDzHhp9iXnjo3XBkXx6qXi06zg5O_LP0D1ouYlBU --claim-rooms d87ab26b-3446-47d9-8756-eea5c724b4b2 --claim-url https://app.netdata.cloud
